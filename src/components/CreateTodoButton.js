ca428a7e1d2b1537d5402e7b562447ad14bbc743
Johan Caicedo <camilo.design07@gmail.com>
2024-03-04 08:22:43 -0500

    Cambios menor en el botón

--- src/components/CreateTodoButton.js
+++ src/components/CreateTodoButton.js
@@ -4,11 +4,15 @@ import '../styles/CreateTodoButton.css';
 function CreateTodoButton() {
     
     return (
-        <div className="overlayContainer">
+        <div>
             <motion.button
                 className="CreateTodoButton"
                 whileHover={{ scale: 1.1, rotate: 90 }} 
                 whileTap={{ scale: 0.9 }}
+
+                    onClick={
+                        () => console.log('le diste click')
+                    }
             >
                 +
             </motion.button>
