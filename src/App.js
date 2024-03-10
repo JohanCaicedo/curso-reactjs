f3e4b97ea086540ef57542a52e832838fb4b38d7
Johan Caicedo <camilo.design07@gmail.com>
2024-03-10 15:41:13 -0500

    ✨ Local Storage
    Local Sotrage basic implementation

--- src/App.js
+++ src/App.js
@@ -7,25 +7,21 @@ import { CreateTodoButton } from "./CreateTodoButton";
 import { TodoItem } from "./TodoItem";
 import "./css/index.css";
 
-const defaultTodos = [
-  { text: "Terminar esta linea", completed: true },
-  { text: "Terminar curso React.js", completed: false },
-  { text: "Hacer los cambios a la pagina de la red", completed: false },
-  { text: "Hacer el portafolio", completed: false },
-  { text: "Comer pastel", completed: false },
-  { text: "Cambiar el cajón", completed: false },
-  { text: "Caminar por la playa", completed: true },
-  { text: "Volar en avión", completed: true },
-  { text: "Uno cuando volé", completed: true },
-];
-
 function App() {
-  const [searchValue, setSearchValue] = React.useState("");
+  const [todos, setTodos] = React.useState(() => {
+    const localStorageTodos = localStorage.getItem('Todos-v2');
+    if (!localStorageTodos) {
+      const initialTodos = [];
+      localStorage.setItem('Todos-v2', JSON.stringify(initialTodos));
+      return initialTodos;
+    } else {
+      return JSON.parse(localStorageTodos);
+    }
+  });
 
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
-
   const totalTodos = todos.length;
 
   const searchedTodos = todos.filter((todo) => {
@@ -34,23 +30,24 @@ function App() {
     return lowerCaseText.includes(lowerCaseSearchValue);
   });
 
+
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('Todos-v2', JSON.stringify(newTodos));
+    setTodos (newTodos);
+
+  }
+
   const doneTodo = (text) => {
     const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-      );
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
-  };
-  const deletedTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-      );
-    newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
+  const deletedTodo = (text) => {
+    const newTodos = todos.filter((todo) => todo.text !== text);
+    saveTodos(newTodos);
+  };
 
   return (
     <React.Fragment>
@@ -63,9 +60,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
-            onCompleted={() =>doneTodo(todo.text)}
-            onDelete={() =>deletedTodo(todo.text)}
-            
+            onCompleted={() => doneTodo(todo.text)}
+            onDelete={() => deletedTodo(todo.text)}
           />
         ))}
       </TodoList>
